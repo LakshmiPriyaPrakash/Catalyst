cc5db397f8227faa0b55137938b739d5b6110cf7
prakashlakshmipriya@gmail.com <prakashlakshmipriya@gmail.com>
2021-10-08 10:43:42 -0700

    coded csrfFetch function

---
+++ frontend/src/store/csrf.js
@@ -0,0 +1,20 @@
+import Cookies from 'js-cookie';
+
+export async function csrfFetch(url, options = {}) {
+
+  options.method = options.method || 'GET';   // set options.method to 'GET' if there is no method
+
+  options.headers = options.headers || {};    // set options.headers to an empty object if there is no headers
+
+  if (options.method.toUpperCase() !== 'GET') {                 // if the options.method is not 'GET',
+    options.headers['Content-Type'] =                           //then set the "Content-Type" header to
+      options.headers['Content-Type'] || 'application/json';    // "application/json",
+    options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');  //and set the "XSRF-TOKEN" header to the value of the "XSRF-TOKEN" cookie
+  }
+
+  const res = await window.fetch(url, options);                 // call the default window's fetch with the url and the options passed in
+
+  if (res.status >= 400) throw res; // if the response status code is 400 or above, then throw an error with the error being the response
+
+  return res;   // if the response status code is under 400, then return the response to the next promise chain
+}
