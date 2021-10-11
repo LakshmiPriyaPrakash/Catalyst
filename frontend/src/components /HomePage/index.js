b573c3300b88d61f36e5342bbf3509fe1a66755b
prakashlakshmipriya@gmail.com <prakashlakshmipriya@gmail.com>
2021-10-10 23:25:21 -0700

    refactored home page to display only for guest/logged out users

--- frontend/src/components /HomePage/index.js
+++ frontend/src/components /HomePage/index.js
@@ -1,27 +1,34 @@
+import { useSelector } from 'react-redux';
 import './HomePage.css'
 
 function Homepage() {
+    const sessionUser = useSelector(state => state.session.user);
 
-    return (
-        <>
-            <div id="hp-misc-div">
-                <p>Some content goes here</p>
-                <img src="https://res.cloudinary.com/lpriya/image/upload/v1633931860/Catalyst/earth_word_bubble_nmx9k1.png"></img>
-            </div>
-            <div id="hp-feed-container">
-            <div id="left-div">
-                left div
-            </div>
-            <div id="center-div">
-                center div containing stories feed
-            </div>
-            <div id="right-div">
-                right div
-            </div>
-            </div>
-        </>
+    if(!sessionUser) {
+        return (
+            <>
+                <div id="hp-misc-div">
+                    <p>Some content goes here</p>
+                    <img src="https://res.cloudinary.com/lpriya/image/upload/v1633931860/Catalyst/earth_word_bubble_nmx9k1.png"></img>
+                </div>
+                <div id="hp-feed-container">
+                <div id="left-div">
+                    left div
+                </div>
+                <div id="center-div">
+                    center div containing stories feed
+                </div>
+                <div id="right-div">
+                    right div
+                </div>
+                </div>
+            </>
 
-      );
+          );
+    } else {
+        return null;
+    }
+
 }
 
 export default Homepage;
