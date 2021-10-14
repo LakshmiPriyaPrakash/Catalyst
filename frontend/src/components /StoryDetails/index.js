6dbe671f9905c09b0c83a663e6cbf2513a783bbc
prakashlakshmipriya@gmail.com <prakashlakshmipriya@gmail.com>
2021-10-13 17:20:40 -0700

    added comments button in story details page and a comments sidebar

--- frontend/src/components /StoryDetails/index.js
+++ frontend/src/components /StoryDetails/index.js
@@ -1,24 +1,37 @@
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import './StoryDetails.css';
 
 function StoryDetail() {
     const { storyId } = useParams();
     const story = useSelector(state => state.stories[storyId]);
+    const [showComments, setShowComments] = useState(false);
+
     if(story) {
         let d = new Date(story.createdAt);
         let dateWritten = d.toString().slice(4, 10)
 
         return (
             <>
-                <div id="story-details">
-                    <p className="story-elements">{story.User.name}</p>
-                    <p className="story-elements">{dateWritten}</p>
-                    <h2 className="story-elements">{story.title}</h2>
-                    <h5 className="story-elements">{story.subtitle}</h5>
-                    <img id="sd-img"src={story.imageUrl} alt="story"/>
-                    <p className="story-elements">{story.body}</p>
-                    <button id="comments-btn" type="submit">Comments</button>
+                <div id="story-comments">
+                    <div id="story-details">
+                        <p className="story-elements">{story.User.name}</p>
+                        <p className="story-elements">{dateWritten}</p>
+                        <h2 className="story-elements">{story.title}</h2>
+                        <h5 className="story-elements">{story.subtitle}</h5>
+                        <img id="sd-img"src={story.imageUrl} alt="story"/>
+                        <p className="story-elements">{story.body}</p>
+                        <button id="comments-btn" type="submit" onClick={() => setShowComments(true)}>
+                            Comments
+                        </button>
+                    </div>
+                    <div className="sidebar"
+                        style={showComments ? { transform: 'translateX(-100%)' }:{}}
+                    >
+                        Does this display?
+                        <button type="submit" onClick={() => setShowComments(false)}>close</button>
+                    </div>
                 </div>
             </>
         )
