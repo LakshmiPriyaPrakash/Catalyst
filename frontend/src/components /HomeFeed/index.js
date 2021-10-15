7cd1cf65d6bf0e93b01dc95df81ebcdf82becd77
prakashlakshmipriya@gmail.com <prakashlakshmipriya@gmail.com>
2021-10-15 01:42:42 -0700

    added css to home feed

--- frontend/src/components /HomeFeed/index.js
+++ frontend/src/components /HomeFeed/index.js
@@ -11,21 +11,30 @@ function HomeFeed() {
     if(storiesArr.length) {
         return (
             <>
-                <h2>All stories</h2>
+                <h2></h2>
                  <ul>
                     {storiesArr.map(story => {
                         let d = new Date(story.createdAt);
                         let dateWritten = d.toString().slice(4, 10)
                         return(
                             <li key={story.id} id="feed-list">
-                                <p>{story.User.name}</p>
-                                <NavLink id="story-link" to={`/stories/${story.id}`}>
-                                    <img id="feed-img" src={story.imageUrl} alt="story"/>
-                                    <h3>{story.title}</h3>
-                                    <h5>{story.subtitle}</h5>
-                                </NavLink>
-                                <p>{dateWritten}</p>
+                                <div className="story-container">
+                                    <div className="story-details">
+                                        <p>{story.User.name}</p>
+                                        <NavLink id="story-link" to={`/stories/${story.id}`}>
+                                            <h2>{story.title}</h2>
+                                            <p id="subtitle">{story.subtitle}</p>
+                                        </NavLink>
+                                        <p>{dateWritten}</p>
+                                    </div>
+                                    <div>
+                                        <NavLink id="story-link" to={`/stories/${story.id}`}>
+                                            <img id="feed-img" src={story.imageUrl} alt="story"/>
+                                        </NavLink>
+                                    </div>
 
+
+                                </div>
                             </li>
                         )
                     })}
