8167e830dfe94252a57361789ceb43f0ab4a6a2d
prakashlakshmipriya@gmail.com <prakashlakshmipriya@gmail.com>
2021-10-15 16:19:08 -0700

    replaced for with htmlFor in form labels

--- frontend/src/components /WriteStory/index.js
+++ frontend/src/components /WriteStory/index.js
@@ -61,7 +61,7 @@ function WriteStory() {
                                 />
                         </div>
                         <div className="ws-form-field">
-                            <label for="story-subtitle">Subtitle</label>
+                            <label htmlFor="story-subtitle">Subtitle</label>
                                 <input
                                 className="sf-input"
                                 id="story-subtitle"
@@ -73,7 +73,7 @@ function WriteStory() {
                                 />
                         </div>
                         <div className="ws-form-field">
-                            <label for="image">Image URL</label>
+                            <label htmlFor="image">Image URL</label>
                                 <input
                                 className="sf-input"
                                 id="image"
@@ -85,7 +85,7 @@ function WriteStory() {
                                 />
                         </div>
                         <div className="ws-form-field">
-                            <label for="content">Content</label>
+                            <label htmlFor="content">Content</label>
                                 <textarea
                                 className="sf-content"
                                 id="content"
