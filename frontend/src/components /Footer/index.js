6d883346bbdfd671053e6be36f8d635bc91e213d
prakashlakshmipriya@gmail.com <prakashlakshmipriya@gmail.com>
2021-10-15 16:11:10 -0700

    added alt text to images

--- frontend/src/components /Footer/index.js
+++ frontend/src/components /Footer/index.js
@@ -7,12 +7,12 @@ function Footer() {
                 <div id="links">
                     <div id="github">
                         <a className="gl-links" href='https://github.com/LakshmiPriyaPrakash'>
-                            <img className="links-logo" src="https://res.cloudinary.com/lpriya/image/upload/v1634337384/Catalyst/github-logo_jbearb.png" />
+                            <img className="links-logo" src="https://res.cloudinary.com/lpriya/image/upload/v1634337384/Catalyst/github-logo_jbearb.png" alt="Github logo"/>
                         </a>
                     </div>
                     <div id="linkedin">
                         <a className="gl-links" href='https://www.linkedin.com/in/lakshmi-priya-prakash/'>
-                            <img className="links-logo" src="https://res.cloudinary.com/lpriya/image/upload/v1634337384/Catalyst/linkedin-logo_f6zqhq.png" />
+                            <img className="links-logo" src="https://res.cloudinary.com/lpriya/image/upload/v1634337384/Catalyst/linkedin-logo_f6zqhq.png" alt="LinkedIn logo"/>
                         </a>
                     </div>
                 </div>
